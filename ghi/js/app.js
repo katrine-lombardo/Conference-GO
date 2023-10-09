baa50b25e51dc0d30f4d40a0f66711e660dcf54c
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-09 14:43:21 -0400

    Completed Data to DOM

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -8,6 +8,23 @@ window.addEventListener("DOMContentLoaded", async () => {
       // Figure out what to do when the response is bad
     } else {
       const data = await response.json();
+      const conference = data.conferences[0];
+
+      // Conference title
+      const nameTag = document.querySelector(".card-title");
+      nameTag.innerHTML = conference.name;
+
+      // Conference details
+      const detailUrl = `http://localhost:8000${conference.href}`;
+      const detailResponse = await fetch(detailUrl);
+
+      if (detailResponse.ok) {
+        const details = await detailResponse.json();
+
+        // Conference description
+        const descriptionTag = document.querySelector(".card-text");
+        descriptionTag.innerHTML = details.conference.description;
+      }
     }
   } catch (e) {
     // Figure out what to do if an error is raised
