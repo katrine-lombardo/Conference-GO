eecb6aa1e76dc55d4e163d73c40f739cfe04e98e
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-09 12:52:13 -0400

    Completed The First Fetch

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,15 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", async () => {
+  const url = "http://localhost:8000/api/conferences/";
 
+  try {
+    const response = await fetch(url);
 
+    if (!response.ok) {
+      // Figure out what to do when the response is bad
+    } else {
+      const data = await response.json();
+    }
+  } catch (e) {
+    // Figure out what to do if an error is raised
+  }
 });
