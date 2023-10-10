5631b6486028ae458aa480dbb94c7ae78f7979ee
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-10 11:45:16 -0400

    added working event listener

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+// Add event listener for when DOM loads
+window.addEventListener("DOMContentLoaded", async () => {
+  // Declare a variable to hold URL for the API
+  const url = "http://localhost:8000/api/states/";
+
+  // Fetch the URL (use await to convert the promise)
+  const response = await fetch(url);
+  console.log(response);
+
+  // If the response is ok then get the data using .json method and await
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data)
+    } else {
+    // Figure out what to do when the response is bad
+    }
+});
