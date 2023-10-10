bd5134841a099b2c5b7a8e066ed7f4a39d40c044
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-10 12:51:46 -0400

    Form now saves all fields (including state)

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -10,34 +10,44 @@ window.addEventListener("DOMContentLoaded", async () => {
   // If the response is ok then get the data using .json method and await
   if (response.ok) {
     const data = await response.json();
-    console.log(data)
-
-    const selectTag = document.getElementById('state')
+    console.log(data);
 
     // ------- STATE DROPDOWN MENU -------
-
-    // Get the select tag element by its id 'state'
-    const stateDiv = document.querySelector("#state");
-    console.log(stateDiv);
-
-
-    // For each state in the states property of the data
+    const selectTag = document.getElementById("state");
     for (let state of data.states) {
-        // Create an 'option' element
-        const option = document.createElement('option')
-
-        // Set the '.value' property of the option element to the state's abbreviation
-        option.value = state.abbreviation
-
-        // Set the '.innerHTML' property of the option element to the state's name
-        option.innerHTML = state.name
-
-        // Append the option element as a child of the select tag
-        selectTag.appendChild(option)
+      const option = document.createElement("option");
+      option.value = state.abbreviation;
+      option.innerHTML = state.name;
+      selectTag.appendChild(option);
     }
 
+    // ------- GET FORM DATA -------
+    const formTag = document.getElementById("create-location-form");
+    formTag.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const formData = new FormData(formTag);
+      const json = JSON.stringify(Object.fromEntries(formData));
+      console.log(json);
+      console.log("need to submit the form data");
 
-    } else {
+      // ------- SEND DATA TO SERVER -------
+      const locationUrl = "http://localhost:8000/api/locations/";
+      const fetchConfig = {
+        method: "post",
+        body: json,
+        headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(locationUrl, fetchConfig);
+    if (response.ok) {
+      formTag.reset();
+      const newLocation = await response.json();
+      console.log(newLocation);
+    }
+    });
+
+  } else {
     // Figure out what to do when the response is bad
-    }
+  }
 });
