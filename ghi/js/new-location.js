6ea945f7f298e5d7684f54cab9ab2cec282e7794
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-10 12:18:49 -0400

    state dropdown works

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -11,6 +11,32 @@ window.addEventListener("DOMContentLoaded", async () => {
   if (response.ok) {
     const data = await response.json();
     console.log(data)
+
+    const selectTag = document.getElementById('state')
+
+    // ------- STATE DROPDOWN MENU -------
+
+    // Get the select tag element by its id 'state'
+    const stateDiv = document.querySelector("#state");
+    console.log(stateDiv);
+
+
+    // For each state in the states property of the data
+    for (let state of data.states) {
+        // Create an 'option' element
+        const option = document.createElement('option')
+
+        // Set the '.value' property of the option element to the state's abbreviation
+        option.value = state.abbreviation
+
+        // Set the '.innerHTML' property of the option element to the state's name
+        option.innerHTML = state.name
+
+        // Append the option element as a child of the select tag
+        selectTag.appendChild(option)
+    }
+
+
     } else {
     // Figure out what to do when the response is bad
     }
