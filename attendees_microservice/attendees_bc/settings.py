8714fd7b711f3d4077b30d74356b39dfab7b8a1e
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-11 15:50:27 -0400

    Completed Get Some Data

--- attendees_microservice/attendees_bc/settings.py
+++ attendees_microservice/attendees_bc/settings.py
@@ -51,6 +51,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
 ]
 
 CRONJOBS = [
--- ghi/attendees/src/index.js
+++ ghi/attendees/src/index.js
@@ -15,3 +15,15 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+async function loadAttendees() {
+  const response = await fetch('http://localhost:8001/api/attendees/');
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data);
+  } else {
+    console.error(response);
+  }
+}
+loadAttendees();
