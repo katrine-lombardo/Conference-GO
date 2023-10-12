92a3c227b34a3d5dbb7ac67c931d220ec3d2aeb3
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-12 15:19:35 -0400

    Completed Saving Form State

--- ghi/app/src/LocationForm.js
+++ ghi/app/src/LocationForm.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from "react";
 
-function LocationForm() {
+function LocationForm(props) {
   const [states, setStates] = useState([]);
 
-  const fetchData = async () => {
-    // Declare a variable to hold URL for the API
-    const url = "http://localhost:8000/api/states/";
+  // Event handlers and methods
+  const [name, setName] = useState([]);
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    setName(value)
+  }
 
-    // Fetch the URL (use await to convert the promise)
+  const [city, setCity] = useState([]);
+  const handleCityChange = (e) => {
+    const value = e.target.value;
+    setCity(value)
+  }
+
+  const [room_count, setRoomCount] = useState([]);
+  const handleRoomCountChange = (e) => {
+    const value = e.target.value;
+    setRoomCount(value)
+  }
+
+  // Fetch data (Declare variable, fetch url, get data using json + await)
+  const fetchData = async () => {
+    const url = "http://localhost:8000/api/states/";
     const response = await fetch(url);
 
-    // If the response is ok then get the data using .json method and await
     if (response.ok) {
       const data = await response.json();
       setStates(data.states);
@@ -25,6 +41,15 @@ function LocationForm() {
     <>
     <p>A location form</p>
     <form>
+    <input onChange={handleNameChange} placeholder="Name" required
+       type="text" name="name" id="name"
+       className="form-control" />
+    <input onChange={handleCityChange} placeholder="City" required
+       type="text" name="city" id="city"
+       className="form-control" />
+    <input onChange={handleRoomCountChange} placeholder="Room Count" required
+       type="number" name="room_count" id="room_count"
+       className="form-control" />
     <select required name="state" id="state" className="form-select">
       <option value="">Choose a state</option>
       {states.map(state => {
