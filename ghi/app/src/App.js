e57228b6cc41af5088cb8b8827ee05ab819711f4
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-12 12:37:26 -0400

    Completed Showing A Form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,30 +1,17 @@
 import Nav from "./Nav";
+import AttendeesList from "./AttendeesList";
 
 function App(props) {
   if (props.attendees === undefined) {
     return null;
   }
   return (
-    <><Nav /><div className="container">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.attendees.map(attendee => {
-            return (
-              <tr key={attendee.href}>
-                <td>{attendee.name}</td>
-                <td>{attendee.conference}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div></>
+    <>
+      <><Nav /></>
+      <div className="container">
+        <><AttendeesList attendees={props.attendees}/></>
+      </div>
+    </>
   );
 }
 
