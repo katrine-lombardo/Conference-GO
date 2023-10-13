6ae4e3696fd659e34e6f8a9d4a742659f79b35a2
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-13 12:19:10 -0400

    Updated NavLinks in Nav.js

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,10 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <NavLink className="navbar-brand" href="#">
           Conference GO!
-        </a>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -19,32 +21,32 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <NavLink className="nav-link active" aria-current="page" href="#">
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="new-location.html">
+              <NavLink className="nav-link" aria-current="page" href="new-location.html">
                 New location
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a
+              <NavLink
                 className="nav-link"
                 aria-current="page"
                 href="new-conference.html"
               >
                 New conference
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a
+              <NavLink
                 className="nav-link"
                 aria-current="page"
                 href="new-presentation.html"
               >
                 New presentation
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
