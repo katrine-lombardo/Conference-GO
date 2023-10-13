8369bfc84a6ee8eecccce9322469c86a908e511e
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-13 11:45:30 -0400

    Added missing attendence files and updated form.js for conference and location

--- ghi/app/src/ConferenceForm.js
+++ ghi/app/src/ConferenceForm.js
@@ -1,195 +1,137 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-function ConferenceForm(props) {
-  // ------- SUBMIT THE FORM -------
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+function ConferenceForm() {
+  const [locations, setLocations] = useState([])
 
-    const data = {};
-    console.log(data)
-    data.name = name;
-    data.description = description;
-    data.max_presentations = max_presentations;
-    data.max_attendees = max_attendees;
-    data.starts = starts;
-    data.ends = ends;
-    data.location = location;
+  //Notice that we can condense all formData
+  //into one state object
+  const [formData, setFormData] = useState({
+    name: '',
+    starts: '',
+    ends: '',
+    description: '',
+    max_presentations: '',
+    max_attendees: '',
+    location: '',
+  })
 
-    const conferenceUrl = "http://localhost:8000/api/conferences/";
-    const fetchConfig = {
-      method: "post",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await fetch(conferenceUrl, fetchConfig);
-    if (response.ok) {
-      const newConference = await response.json();
-      console.log(newConference);
-      setName("");
-      setDescription("");
-      setMaxPresentations("");
-      setMaxAttendees("");
-      setStarts("");
-      setEnds("");
-      setLocation("");
-    }
-  };
-
-  // ------- USER INPUT STATE -------
-
-  const [location, setLocation] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [max_presentations, setMaxPresentations] = useState("");
-  const [max_attendees, setMaxAttendees] = useState("");
-  const [starts, setStarts] = useState("");
-  const [ends, setEnds] = useState("");
-
-  // ------- EVENT HANDLERS -------
-
-  const handleLocationChange = (e) => {
-    const value = e.target.value;
-    setLocation(value);
-  };
-  const handleNameChange = (e) => {
-    const value = e.target.value;
-    setName(value);
-  };
-  const handleDescriptionChange = (e) => {
-    const value = e.target.value;
-    setDescription(value);
-  };
-  const handleMaxPresentationsChange = (e) => {
-    const value = e.target.value;
-    setMaxPresentations(value);
-  };
-  const handleMaxAttendeesChange = (e) => {
-    const value = e.target.value;
-    setMaxAttendees(value);
-  };
-  const handleStartsChange = (e) => {
-    const value = e.target.value;
-    setStarts(value);
-  };
-  const handleEndsChange = (e) => {
-    const value = e.target.value;
-    setEnds(value);
-  };
-
-  // ------- FETCH DATA FOR LOCATIONS -------
-  const [locations, setLocations] = useState([]);
   const fetchData = async () => {
-    const url = "http://localhost:8000/api/locations/";
+    const url = 'http://localhost:8000/api/locations/';
     const response = await fetch(url);
-
     if (response.ok) {
       const data = await response.json();
       setLocations(data.locations);
     }
-  };
+  }
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  // ------- FORM -------
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const url = 'http://localhost:8000/api/conferences/';
+
+    const fetchConfig = {
+      method: "post",
+      //Because we are using one formData state object,
+      //we can now pass it directly into our request!
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(url, fetchConfig);
+
+    if (response.ok) {
+      //The single formData object
+      //also allows for easier clearing of data
+      setFormData({
+        name: '',
+        starts: '',
+        ends: '',
+        description: '',
+        max_presentations: '',
+        max_attendees: '',
+        location: '',
+      });
+    }
+  }
+
+  //Notice that we can also replace multiple form change
+  //eventListener functions with one
+  const handleFormChange = (e) => {
+    const value = e.target.value;
+    const inputName = e.target.name;
+
+    //We can condense our form data event handling
+    //into on function by using the input name to update it
+
+    setFormData({
+      //Previous form data is spread (i.e. copied) into our new state object
+      ...formData,
+
+      //On top of the that data, we add the currently engaged input key and value
+      [inputName]: value
+    });
+  }
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="offset-3 col-6">
-          <div className="shadow p-4 mt-4">
-            <h1>Create a new conference</h1>
-            <form onSubmit={handleSubmit} id="create-conference-form">
-              <input
-                value={name}
-                onChange={handleNameChange}
-                placeholder="Name"
-                required
-                type="text"
-                name="name"
-                id="name"
-                className="form-control"
-              />
-              <input
-                value={starts}
-                onChange={handleStartsChange}
-                placeholder="Start date"
-                required
-                type="date"
-                name="starts"
-                id="starts"
-                className="form-control"
-              />
-              <input
-                value={ends}
-                onChange={handleEndsChange}
-                placeholder="End date"
-                required
-                type="date"
-                name="ends"
-                id="ends"
-                className="form-control"
-              />
-              <input
-                value={description}
-                onChange={handleDescriptionChange}
-                placeholder="Description"
-                required
-                type="textarea"
-                name="description"
-                id="description"
-                className="form-control"
-              />
-              <input
-                value={max_presentations}
-                onChange={handleMaxPresentationsChange}
-                placeholder="Maximum presentations"
-                required
-                type="number"
-                name="max_presentations"
-                id="max_presentations"
-                className="form-control"
-              />
-              <input
-                value={max_attendees}
-                onChange={handleMaxAttendeesChange}
-                placeholder="Maximum attendees"
-                required
-                type="number"
-                name="max_attendees"
-                id="max_attendees"
-                className="form-control"
-              />
-              <select
-                value={location}
-                onChange={handleLocationChange}
-                required
-                id="location"
-                name="location"
-                className="form-select"
-              >
+    <div className="row">
+      <div className="offset-3 col-6">
+        <div className="shadow p-4 mt-4">
+          <h1>Create a new conference</h1>
+          <form onSubmit={handleSubmit} id="create-conference-form">
+
+            <div className="form-floating mb-3">
+              {/* !-- Now, each field in our form references the same function -- */}
+              <input onChange={handleFormChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+              <label htmlFor="name">Name</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input onChange={handleFormChange} placeholder="Starts" required type="date" name="starts" id="starts" className="form-control" />
+              <label htmlFor="starts">Starts</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input onChange={handleFormChange} placeholder="Ends" required type="date" name="ends" id="ends" className="form-control" />
+              <label htmlFor="ends">Ends</label>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="description">Description</label>
+              <textarea onChange={handleFormChange} id="description" rows="3" name="description" className="form-control"></textarea>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input onChange={handleFormChange} placeholder="Maximum presentations" required type="number" name="max_presentations" id="max_presentations" className="form-control" />
+              <label htmlFor="max_presentations">Maximum presentations</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input onChange={handleFormChange} placeholder="Maximum attendees" required type="number" name="max_attendees" id="max_attendees" className="form-control" />
+              <label htmlFor="max_attendees">Maximum attendees</label>
+            </div>
+
+            <div className="mb-3">
+              <select onChange={handleFormChange} required name="location" id="location" className="form-select">
                 <option value="">Choose a location</option>
-                {locations.map((location) => {
+                {locations.map(location => {
                   return (
-                    <option
-                    key={location.id}
-                    value={location.id}>
-                      {location.name}
-                    </option>
-                  );
+                    <option key={location.id} value={location.id}>{location.name}</option>
+                  )
                 })}
               </select>
-              <button onClick={handleSubmit} className="btn btn-primary">
-                Create
-              </button>
-            </form>
-          </div>
+            </div>
+            <button className="btn btn-primary">Create</button>
+          </form>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default ConferenceForm;
--- ghi/app/src/LocationForm.js
+++ ghi/app/src/LocationForm.js
@@ -1,144 +1,116 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState } from 'react';
 
-function LocationForm(props) {
-
-  // ------- SUBMIT THE FORM -------
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const data = {};
-    data.room_count = room_count;
-    data.name = name;
-    data.city = city;
-    data.state = state;
-
-    const locationUrl = "http://localhost:8000/api/locations/";
-    const fetchConfig = {
-      method: "post",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await fetch(locationUrl, fetchConfig);
-    if (response.ok) {
-      const newLocation = await response.json();
-      console.log(newLocation)
-      setName("");
-      setRoomCount("");
-      setCity("");
-      setState("");
-    }
-  };
-
-  // ------- USER INPUT STATE -------
-  const [state, setState] = useState([]);
-  const [name, setName] = useState("");
-  const [city, setCity] = useState("");
-  const [room_count, setRoomCount] = useState("");
-
-  // ------- EVENT HANDLERS -------
-  const handleStateChange = (e) => {
-    const value = e.target.value;
-    setState(value);
-  };
-  const handleNameChange = (e) => {
-    const value = e.target.value;
-    setName(value);
-  };
-  const handleCityChange = (e) => {
-    const value = e.target.value;
-    setCity(value);
-  };
-  const handleRoomCountChange = (e) => {
-    const value = e.target.value;
-    setRoomCount(value);
-  };
-
-  // ------- FETCH DATA FOR STATES -------
+function LocationForm () {
+  // Here is the old way of creating state hooks for every
+  // property. Can you refactor this to make it into a single
+  // data object like the ConferenceForm() above?
+  const [name, setName] = useState('');
+  const [city, setCity] = useState('');
+  const [roomCount, setRoomCount] = useState('');
+  const [state, setState] = useState('')
   const [states, setStates] = useState([]);
+
   const fetchData = async () => {
-    const url = "http://localhost:8000/api/states/";
+    const url = 'http://localhost:8000/api/states/';
+
     const response = await fetch(url);
 
     if (response.ok) {
       const data = await response.json();
       setStates(data.states);
     }
-  };
+  }
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  // ------- FORM -------
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {};
+    data.room_count = roomCount;
+    data.name = name;
+    data.city = city;
+    data.state = state;
+
+    const locationUrl = 'http://localhost:8000/api/locations/';
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(locationUrl, fetchConfig);
+    if (response.ok) {
+      const newLocation = await response.json();
+      console.log(newLocation);
+
+      setName('');
+      setRoomCount('');
+      setCity('');
+      setState('');
+    }
+  }
+
+  // How can we refactor these handleChange methods to make
+  // a single method, like the ConferenceForm above?
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+  }
+
+  const handleRoomCountChange = (event) => {
+    const value = event.target.value;
+    setRoomCount(value);
+  }
+
+  const handleCityChange = (event) => {
+    const value = event.target.value;
+    setCity(value);
+  }
+
+  const handleStateChange = (event) => {
+    const value = event.target.value;
+    setState(value);
+  }
+
   return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="offset-3 col-6">
-            <div className="shadow p-4 mt-4">
-              <h1>Create a new location</h1>
-              <form onSubmit={handleSubmit} id="create-location-form">
-                <input
-                  value={name}
-                  onChange={handleNameChange}
-                  placeholder="Name"
-                  required
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="form-control"
-                />
-                <input
-                  value={city}
-                  onChange={handleCityChange}
-                  placeholder="City"
-                  required
-                  type="text"
-                  name="city"
-                  id="city"
-                  className="form-control"
-                />
-                <input
-                  value={room_count}
-                  onChange={handleRoomCountChange}
-                  placeholder="Room Count"
-                  required
-                  type="number"
-                  name="room_count"
-                  id="room_count"
-                  className="form-control"
-                />
-                <select
-                  value={state}
-                  onChange={handleStateChange}
-                  required
-                  name="state"
-                  id="state"
-                  className="form-select"
-                >
-                  <option value="">Choose a state</option>
-                  {states.map((state) => {
-                    return (
-                      <option
-                        key={state.abbreviation}
-                        value={state.abbreviation}
-                      >
-                        {state.name}
-                      </option>
-                    );
-                  })}
-                </select>
-                <button onClick={handleSubmit} className="btn btn-primary">
-                  Create
-                </button>
-              </form>
+    <div className="row">
+      <div className="offset-3 col-6">
+        <div className="shadow p-4 mt-4">
+          <h1>Create a new location</h1>
+          <form onSubmit={handleSubmit} id="create-location-form">
+            <div className="form-floating mb-3">
+              <input value={name} onChange={handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+              <label htmlFor="name">Name</label>
             </div>
-          </div>
+            <div className="form-floating mb-3">
+              <input value={roomCount} onChange={handleRoomCountChange} placeholder="Room count" required type="number" name="room_count" id="room_count" className="form-control" />
+              <label htmlFor="room_count">Room count</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input value={city} onChange={handleCityChange} placeholder="City" required type="text" name="city" id="city" className="form-control" />
+              <label htmlFor="city">City</label>
+            </div>
+            <div className="mb-3">
+              <select value={state} onChange={handleStateChange} required name="state" id="state" className="form-select">
+                <option value="">Choose a state</option>
+                {states.map(state => {
+                  return (
+                    <option key={state.abbreviation} value={state.abbreviation}>
+                      {state.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <button className="btn btn-primary">Create</button>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
