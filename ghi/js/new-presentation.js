447f315b519711fc321017699c6081832dfe76d1
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-13 12:57:23 -0400

    Replaced new presentation .js and .html

--- ghi/js/new-presentation.js
+++ ghi/js/new-presentation.js
@@ -1,51 +1,42 @@
-// Add event listener for when DOM loads
-window.addEventListener("DOMContentLoaded", async () => {
-  // Declare a variable to hold URL for the API
-  const url = `http://localhost:8000/api/conferences/`;
+window.addEventListener('DOMContentLoaded', async () => {
 
-  // Fetch the URL (use await to convert the promise)
+  const url = 'http://localhost:8000/api/conferences/';
+
   const response = await fetch(url);
 
-  // If the response is ok then get the data using .json method and await
   if (response.ok) {
     const data = await response.json();
 
-    // ------- CONFERENCE DROPDOWN MENU -------
-    const selectTag = document.getElementById("conference");
+    const selectTag = document.getElementById('conference');
     for (let conference of data.conferences) {
-      const option = document.createElement("option");
+      const option = document.createElement('option');
       option.value = conference.id;
       option.innerHTML = conference.name;
       selectTag.appendChild(option);
     }
+  }
 
-    // ------- GET FORM DATA -------
-    const formTag = document.getElementById("create-presentation-form");
-    formTag.addEventListener("submit", async (event) => {
-      event.preventDefault();
-      const formData = new FormData(formTag);
-      const json = JSON.stringify(Object.fromEntries(formData));
-      console.log(json);
-      console.log("need to submit the form data");
+  const formTag = document.getElementById('create-presentation-form');
+  const selectTag = document.getElementById('conference');
+  formTag.addEventListener('submit', async event => {
+    event.preventDefault();
+    const formData = new FormData(formTag);
+    const json = JSON.stringify(Object.fromEntries(formData));
 
-      // ------- SEND DATA TO SERVER -------
-      const presentationUrl = `http://localhost:8000/api/conferences/${formData.get("conference")}/presentations/`;
-      const fetchConfig = {
-        method: "post",
-        body: json,
-        headers: {
-        "Content-Type": "application/json",
+    const conferenceId = selectTag.options[selectTag.selectedIndex].value;
+    const locationUrl = `http://localhost:8000/api/conferences/${conferenceId}/presentations/`;
+    const fetchConfig = {
+      method: "post",
+      body: json,
+      headers: {
+        'Content-Type': 'application/json',
       },
     };
-    const response = await fetch(presentationUrl, fetchConfig);
+    const response = await fetch(locationUrl, fetchConfig);
     if (response.ok) {
       formTag.reset();
-      const newPresentation = await response.json();
-      console.log(newPresentation);
+      const newConference = await response.json();
+      console.log(newConference);
     }
-    });
-
-  } else {
-    // Figure out what to do when the response is bad
-  }
+  });
 });
