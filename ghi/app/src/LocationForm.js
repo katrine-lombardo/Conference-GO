abcd79a40c98437901a893d87dd4b380e2e4bb50
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-16 10:40:45 -0400

    Refactored LocationForm event handlers

--- ghi/app/src/LocationForm.js
+++ ghi/app/src/LocationForm.js
@@ -4,17 +4,18 @@ function LocationForm () {
   // Here is the old way of creating state hooks for every
   // property. Can you refactor this to make it into a single
   // data object like the ConferenceForm() above?
-  const [name, setName] = useState('');
-  const [city, setCity] = useState('');
-  const [roomCount, setRoomCount] = useState('');
-  const [state, setState] = useState('')
   const [states, setStates] = useState([]);
+
+  const[formData, setFormData] = useState({
+    name: '',
+    city: '',
+    room_count: '',
+    state: '',
+  })
 
   const fetchData = async () => {
     const url = 'http://localhost:8000/api/states/';
-
     const response = await fetch(url);
-
     if (response.ok) {
       const data = await response.json();
       setStates(data.states);
@@ -25,55 +26,39 @@ function LocationForm () {
     fetchData();
   }, []);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const data = {};
-    data.room_count = roomCount;
-    data.name = name;
-    data.city = city;
-    data.state = state;
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const locationUrl = 'http://localhost:8000/api/locations/';
     const fetchConfig = {
       method: "post",
-      body: JSON.stringify(data),
+      body: JSON.stringify(formData),
       headers: {
         'Content-Type': 'application/json',
       },
     };
     const response = await fetch(locationUrl, fetchConfig);
-    if (response.ok) {
-      const newLocation = await response.json();
-      console.log(newLocation);
 
-      setName('');
-      setRoomCount('');
-      setCity('');
-      setState('');
+
+    if (response.ok) {
+      setFormData({
+        name: '',
+        city: '',
+        roomCount: '',
+        state: '',
+      })
     }
   }
 
   // How can we refactor these handleChange methods to make
   // a single method, like the ConferenceForm above?
-  const handleNameChange = (event) => {
-    const value = event.target.value;
-    setName(value);
-  }
+  const handleFormChange = (e) => {
+    const value = e.target.value;
+    const inputName = e.target.name;
 
-  const handleRoomCountChange = (event) => {
-    const value = event.target.value;
-    setRoomCount(value);
-  }
-
-  const handleCityChange = (event) => {
-    const value = event.target.value;
-    setCity(value);
-  }
-
-  const handleStateChange = (event) => {
-    const value = event.target.value;
-    setState(value);
+    setFormData({
+      ...formData,
+      [inputName]: value
+    })
   }
 
   return (
@@ -83,19 +68,19 @@ function LocationForm () {
           <h1>Create a new location</h1>
           <form onSubmit={handleSubmit} id="create-location-form">
             <div className="form-floating mb-3">
-              <input value={name} onChange={handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+              <input value={formData.name} onChange={handleFormChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
               <label htmlFor="name">Name</label>
             </div>
             <div className="form-floating mb-3">
-              <input value={roomCount} onChange={handleRoomCountChange} placeholder="Room count" required type="number" name="room_count" id="room_count" className="form-control" />
+              <input value={formData.roomCount} onChange={handleFormChange} placeholder="Room count" required type="number" name="room_count" id="room_count" className="form-control" />
               <label htmlFor="room_count">Room count</label>
             </div>
             <div className="form-floating mb-3">
-              <input value={city} onChange={handleCityChange} placeholder="City" required type="text" name="city" id="city" className="form-control" />
+              <input value={formData.city} onChange={handleFormChange} placeholder="City" required type="text" name="city" id="city" className="form-control" />
               <label htmlFor="city">City</label>
             </div>
             <div className="mb-3">
-              <select value={state} onChange={handleStateChange} required name="state" id="state" className="form-select">
+              <select value={formData.state} onChange={handleFormChange} required name="state" id="state" className="form-select">
                 <option value="">Choose a state</option>
                 {states.map(state => {
                   return (
